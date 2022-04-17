db7c97344efd97bafce85157a99963e76a32b489
Mohammad Eskandari <good_teacher_ey@hotmail.com>
2022-04-17 16:31:12 +0430

    first commit

---
+++ src/KafkaConnector.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kafka;
+
+use Illuminate\Queue\Connectors\ConnectorInterface;
+
+class KafkaConnector implements ConnectorInterface
+{
+
+    public function connect(array $config)
+    {
+        $conf = new \RdKafka\Conf();
+
+        $conf->set('bootstrap.servers', $config['bootstrap.servers']);
+        $conf->set('security.protocol', $config['security.protocol']);
+        $conf->set('sasl.mechanism', $config['sasl.mechanism']);
+        $conf->set('sasl.username', $config['sasl.username']);
+        $conf->set('sasl.password', $config['sasl.password']);+
+         $conf->set('group.id', $config['group.id']);
+        $conf->set('auto.offset.reset', 'earliest');
+
+        $consumer = new \RdKafka\KafkaConsumer($conf);
+
+        return new KafkaQueue($consumer);
+    }
+}
